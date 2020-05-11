911ee7c48d3c46c8d240f428f6478925c76d70b1
seeheee <53335160+seeheee@users.noreply.github.com>
2020-05-11 17:59:23 +0900

    최종

---
+++ homework2/Level 3_last/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.use('/api', require('./api'));
+router.use('/api/blog', require('./api/blog'));
+app.use('/',router);
+module.exports = router;
