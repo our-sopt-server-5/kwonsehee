253c433b6961b501b38acab307dc10f67c3170f2
seeheee <53335160+seeheee@users.noreply.github.com>
2020-05-30 00:48:16 +0900

    level23

---
+++ homework1/level2/json.js
@@ -0,0 +1,8 @@
+var member =[
+    {name : '허정민', age : "20", nickname : "wuio"},
+    {name : '정효원', age : "20", nickname : "quiwdh"},
+    {name : '김보배', age : "20", nickname : "ausdy"},
+    {name : '오태진', age : "20", nickname : "qwyo"}
+];
+
+console.log(member);
---
+++ homework1/level3/json.js
@@ -0,0 +1,10 @@
+var member =[
+    {name : '허정민', age : "20", nickname : "wuio"},
+    {name : '정효원', age : "20", nickname : "quiwdh"},
+    {name : '김보배', age : "20", nickname : "ausdy"},
+    {name : '오태진', age : "20", nickname : "qwyo"}
+];
+
+member.forEach(
+    member => console.log('이름은 ' + member.name , '나이는 ' + member.age + '별명은' + member.nickname + '입니다.' )
+)
